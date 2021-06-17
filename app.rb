7b27e29ede9dad930922b0c55806a366ba4d3244
Goncalo Gordo <goncalogordo@gmail.com>
2021-06-17 16:01:58 +0100

    start game with two player instances

--- app.rb
+++ app.rb
@@ -14,23 +14,23 @@ class Battle < Sinatra::Base
   end
 
   get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @player_1_name = $game.player_1.name
+    @player_2_name = $game.player_2.name
     erb :play
   end
 
   post '/names' do
-    $game = Game.new
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(player_1, player_2)
     redirect '/play'
   end
 
   get '/attack' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    $game.attack($player_2)
-    @player_2_hp = $player_2.hp
+    @player_1_name = $game.player_1.name
+    @player_2_name = $game.player_2.name
+    $game.attack($game.player_2)
+    @player_2_hp = $game.player_2.hp
     erb :attack
   end
   
--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -1,10 +1,17 @@
 describe Game do
-  game = Game.new
-  player = Player.new("Goncalo")
+  let ( :player_1)  { Player.new("Emily") }
+  let ( :player_2)  { Player.new("Ziggy") }
+  let ( :game ) { Game.new(player_1, player_2) }
+
+  it 'is initialized with two players' do
+    expect(game.player_1).to eq player_1
+    expect(game.player_2).to eq player_2
+  end
+
   describe '#attack' do
     it 'reduces hp by 10' do
-      game.attack(player)
-      expect(player.hp).to eq 40
+      game.attack(player_2)
+      expect(player_2.hp).to eq 40
     end
   end
 end
